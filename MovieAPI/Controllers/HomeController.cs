456f12f9707c020a3167911e5d6189368cb8b06a
Søren Gjedsøe Eriksen <soer33b0@edu.eal.dk>
2019-10-28 13:29:55 +0100

    Add project files.

---
+++ MovieAPI/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MovieAPI.Models;
+using System.Net.Http;
+using System.Net;
+
+namespace MovieAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly HttpClient client = new HttpClient();
+        [HttpGet]
+        public IActionResult Index()
+        {
+            IEnumerable<Movie> movies = new List<Movie>();
+            using (WebClient client = new WebClient())
+            {
+                var json = client.DownloadString("http://simonsmoviebooking.azurewebsites.net/api/movie");
+                movies = Newtonsoft.Json.JsonConvert.DeserializeObject<IEnumerable<Movie>>(json);
+            }
+            return View(movies);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ReserveTicket(int id)
+        {
+            HttpRequestMessage req = new HttpRequestMessage(HttpMethod.Put, "http://simonsmoviebooking.azurewebsites.net/api/movie/BookMovie/" + id.ToString());
+            using (var client = new HttpClient())
+            {
+                await client.SendAsync(req);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MovieAPI/Models/Movie.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieAPI.Models
+{
+    public class Movie
+    {
+
+        [JsonProperty("id")]
+        public int MovieID
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("title")]
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("genre")]
+        public string Genre
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("description")]
+        public string Description
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("numSeats")]
+        public int Numseats
+        {
+            get;
+            set;
+        }
+    }
+}
